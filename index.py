e7112849f39d7b93991e9e045ad51d76ae893e92
Kalumba Samuel <kalumbasml@gmail.com>
2023-01-04 15:55:58 +0300

    displaying name & format

--- index.py
+++ index.py
@@ -2,7 +2,9 @@ from PIL import Image
 #open image using Image module
 im = Image.open("christmas.png")
 #show actual image
-im.show()
+#im.show()
 #show rotated image
-im = im.rotate(45)
-im.show()
+#im = im.rotate(45)
+#im.show()
+print(im.filename)
+print(im.format)
